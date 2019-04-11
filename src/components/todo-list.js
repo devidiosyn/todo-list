724ceadc77c6ceb767a5005398ab188925815ca0
Vadim <devidiosyn@gmail.com>
2019-04-11 08:03:53 +0300

    Added styles to components

--- src/components/todo-list.js
+++ src/components/todo-list.js
@@ -1,21 +1,24 @@
 import React from 'react';
 import TodoListItem from './todo-list-item';
+import './todo-list.css';
 
 const TodoList = ({ todos }) => {
 
   const elements = todos.map((item) => {
+
+    const { id, ...itemProps } = item;
     return (
-      <li>
-        <TodoListItem { ...item } />
+      <li key={ id } className="list-group-item">
+        <TodoListItem { ...itemProps } />
       </li>
-    )
-  })
+    );
+  });
 
   return (
-    <ul>
+    <ul className="list-group todo-list">
       { elements }
     </ul>
-  )
-}
+  );
+};
 
 export default TodoList;
